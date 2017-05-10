6761d634d5ce3d937bfe789fa2fd3b6b21b0f911
Spartacvz <oc6144yh@go.minneapolis.edu>
2017-05-10 17:11:45 -0500

    class presentation

--- src/main/java/com/Greg/Main.java
+++ src/main/java/com/Greg/Main.java
@@ -8,7 +8,7 @@ public class Main {
         com.Greg.ToDoList List = new com.Greg.ToDoList();
 
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
             // Get a connection to database
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/task_list", "root", "number21");
 
@@ -18,14 +18,15 @@ public class Main {
             // Execute SQL query
             ResultSet mySet = state.executeQuery("SELECT * FROM all_tasks");
 
+            //
 
 
         }
-        catch (ClassNotFoundException cnfe)    {
-            System.out.println("Can't instantiate driver class; check you have drives and classpath configured   correctly?");
-            cnfe.printStackTrace();
-            System.exit(-1); //No driver? Need to fix before anything else will work. So quit the program
-        }
+//        catch (ClassNotFoundException cnfe)    {
+//            System.out.println("Can't instantiate driver class; check you have drives and classpath configured   correctly?");
+//            cnfe.printStackTrace();
+//            System.exit(-1); //No driver? Need to fix before anything else will work. So quit the program
+//        }
         catch (Exception e)    {
             e.printStackTrace();
         }
